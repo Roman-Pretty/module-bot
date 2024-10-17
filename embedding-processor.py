2b295f3be30d1fe4d85dd76d3b2581b1b3d676cc
Roman Pretty <rjjpretty@gmail.com>
2024-10-17 13:50:38 +0100

    Switched to FAISS searching for embeddings due to its faster speed
    Switched from OpenAI 'queries' to 'Chat', which saves conversation memory

--- embedding-processor.py
+++ embedding-processor.py
@@ -1,70 +1,33 @@
-import pandas as pd
+import faiss
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, Settings, StorageContext
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.vector_stores.faiss import FaissVectorStore
 from dotenv import load_dotenv
-from PyPDF2 import PdfReader
-import re
-from transformers import GPT2TokenizerFast
-from openai import OpenAI
 import os
+from datetime import datetime
 
-# Load environment variables from .env file
 load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-# Set up OpenAI client with API key
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+d = 1536 # dimensions of text-ada-embedding-002
+faiss_index = faiss.IndexFlatL2(d)
 
-# Read the PDF file
-reader = PdfReader("UG_Guide.pdf")
-pdf_text = ""
+reader = SimpleDirectoryReader(input_dir="data")
+documents = reader.load_data()
 
-# Extract text from each page of the PDF
-for page in reader.pages:
-    pdf_text += page.extract_text() + "\n"
+Settings.llm = OpenAI(model="gpt-4o-mini", api_key=OPENAI_API_KEY)
+Settings.embed_model = OpenAIEmbedding(model="text-embedding-ada-002")
 
-# Create a dictionary to store the section documents
-section_dict = {}
+vector_store = FaissVectorStore(faiss_index=faiss_index)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+index = VectorStoreIndex.from_documents(documents, storage_context=storage_context, show_progress=True)
 
-# Iterate through each page of the PDF document
-for i, page in enumerate(reader.pages):
-    text = page.extract_text()
-    text = re.sub(r'[^\w\s]', '', text)  # Remove punctuation
-    section_dict[i] = text
+chat_engine = index.as_chat_engine(similarity_top_k=15, chat_mode='context')
 
-# Create a pandas DataFrame with the documents
-dataframe = pd.DataFrame.from_dict(section_dict, orient='index', columns=['content'])
-
-# Create a new column 'heading' with the first five words of each section
-dataframe['heading'] = dataframe['content'].str.extract(r'((?:\w+\W+){0,1}\w+)')
-
-# Create a new column 'title' with the document title
-dataframe['title'] = 'UG_Guide'
-
-# Reorder columns
-columns_titles = ["title", "heading", "content"]
-dataframe = dataframe.reindex(columns=columns_titles)
-dataframe.index.name = 'page'
-
-# Create a tokenizer
-tokenizer = GPT2TokenizerFast.from_pretrained('gpt2')
-
-# Define a function to count the number of tokens in a text string
-def count_tokens(text: str, tokenizer: GPT2TokenizerFast) -> int:
-    return len(tokenizer.encode(text, add_special_tokens=False))
-
-# Count the number of tokens in the 'content' column of the dataframe
-dataframe['tokens'] = dataframe['content'].apply(lambda x: count_tokens(x, tokenizer))
-
-# Define the get_embedding function to retrieve embeddings using OpenAI API
-def get_embedding(text: str) -> list:
-    try:
-        response = client.embeddings.create(model='text-embedding-ada-002',
-        input=text)
-        return response.data[0].embedding  # Return the embedding vector
-    except Exception as e:
-        print(f"Error getting embedding: {e}")
-        return None  # Return None if there's an error
-
-# Apply the get_embedding function to the 'content' column of the dataframe
-dataframe['embedding'] = dataframe['content'].apply(get_embedding)
-
-# Save the DataFrame to a CSV file
-dataframe.to_csv('ug-embeddings.csv', index=True)
+msg = ""
+while msg != "exit":
+    msg = input("You: ")
+    response = chat_engine.chat("You are only allowed to answer questions based on the provided embeddings."
+                                f"today's date is {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}. " + msg)
+    print("Chatbot: ", response)
