8719d4d0070d3b80854845700d1c063df5bd087a
Roman Pretty <rjjpretty@gmail.com>
2025-01-20 22:43:36 +0000

    Trying it without langchain-unstructured to see if it can build

--- api/views/generate_module.py
+++ api/views/generate_module.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse, HttpResponseBadRequest
 from langchain_openai import OpenAIEmbeddings
 from rest_framework.decorators import api_view
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_unstructured import UnstructuredLoader
+# from langchain_unstructured import UnstructuredLoader
 from api.models import Module, ModuleEmbedding, ModuleMember
 from api.llm.selenium import get_qmplus_cookies
 from backend import settings
@@ -46,18 +46,18 @@ def generate_module(request):
     loader.requests_kwargs = {"cookies": cookies}
     documents = loader.load()
 
-    for file in files:
-        file_stream = io.BytesIO()
-        file_stream.write(file.read())
-        file_stream.seek(0)
-
-        # Provide a metadata_filename to indicate the file type
-        loader = UnstructuredLoader(file=file_stream, metadata_filename=file.name)
-        file_documents = loader.load()
-
-        # Append loaded documents to the main list
-        for document in file_documents:
-            documents.append(document)
+    # for file in files:
+    #     file_stream = io.BytesIO()
+    #     file_stream.write(file.read())
+    #     file_stream.seek(0)
+    #
+    #     # Provide a metadata_filename to indicate the file type
+    #     loader = UnstructuredLoader(file=file_stream, metadata_filename=file.name)
+    #     file_documents = loader.load()
+    #
+    #     # Append loaded documents to the main list
+    #     for document in file_documents:
+    #         documents.append(document)
 
     embeddings_function = OpenAIEmbeddings(api_key=settings.OPENAI_API_KEY)
     documents_content = [document.page_content for document in documents]
@@ -97,18 +97,18 @@ def regenerate_module(request):
     loader.requests_kwargs = {"cookies": cookies}
     documents = loader.load()
 
-    for file in files:
-        file_stream = io.BytesIO()
-        file_stream.write(file.read())
-        file_stream.seek(0)
-
-        # Provide a metadata_filename to indicate the file type
-        loader = UnstructuredLoader(file=file_stream, metadata_filename=file.name)
-        file_documents = loader.load()
-
-        # Append loaded documents to the main list
-        for document in file_documents:
-            documents.append(document)
+    # for file in files:
+    #     file_stream = io.BytesIO()
+    #     file_stream.write(file.read())
+    #     file_stream.seek(0)
+    #
+    #     # Provide a metadata_filename to indicate the file type
+    #     loader = UnstructuredLoader(file=file_stream, metadata_filename=file.name)
+    #     file_documents = loader.load()
+    #
+    #     # Append loaded documents to the main list
+    #     for document in file_documents:
+    #         documents.append(document)
 
     embeddings_function = OpenAIEmbeddings(api_key=settings.OPENAI_API_KEY)
     documents_content = [document.page_content for document in documents]
